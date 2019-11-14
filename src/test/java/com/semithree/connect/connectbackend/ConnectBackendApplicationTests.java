f99191d40e51f97c2617daa00d745a342a489098
Chen Yang <chen@pporn.tv>
2019-11-13 21:23:32 -0600

    add SpringBoot barebone

---
+++ src/test/java/com/semithree/connect/connectbackend/ConnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semithree.connect.connectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
